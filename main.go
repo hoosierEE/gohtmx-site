49a3ccfd2e62c5aa981c61859baace900668a112
Alex Shroyer <ashroyer@gmail.com>
2024-08-20 12:04:08 -0400

    pgsql hello world

--- main.go
+++ main.go
@@ -1,19 +1,23 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"html/template"
 	"io"
 	"log"
 	"net/http"
-)
 
-type Templates struct {
-	templates *template.Template
-}
+	"github.com/jackc/pgx/v5"
+)
 
 type UserComment struct {
 	User    string
 	Comment string
+}
+
+type Templates struct {
+	templates *template.Template
 }
 
 // this is why we name our templates
@@ -26,15 +30,29 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/s/", http.StripPrefix("/s/", fileServer))
 
+	// pg
+	urlExample := "postgres://ashroyer-admin@localhost:5432/ashroyer-admin"
+	conn, err := pgx.Connect(context.Background(), urlExample)
+	if err != nil {
+		log.Fatal("Unable to connect to db:", err)
+	}
+	defer conn.Close(context.Background())
+
+	var greeting string
+	err = conn.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
+	if err != nil {
+		log.Fatal("QueryRow failed: ", err)
+	}
+	fmt.Println(greeting)
+
 	// pre-populate some hard-coded data (TODO: from db)
 	comments := []UserComment{
 		{User: "alex", Comment: "hi"},
 		{User: "prateek", Comment: "what's up?"},
 	}
 
-	// parse all templates up front
-	ts := &Templates{
-		templates: template.Must(template.ParseGlob("views/*.html")),
+	ts := &Templates{ // parse all templates up front
+		template.Must(template.ParseGlob("views/*.html")),
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
